61b63a2ab1c386845599e2a89aaa7b4cd3bf79d4
Eritropene <michele.ravaioli3@studio.unibo.it>
2022-11-16 12:49:17 +0100

    make Task a functional interface

--- include/Task.h
+++ include/Task.h
@@ -3,21 +3,9 @@
 
 /*Rappresenta un Task eseguibile dallo Scheduler*/
 class Task {
-
-    unsigned long t = 0;
-    
-private:
-    bool canRun();
-
 public:
-    /*L'azione da eseguire quando il task e' attivo*/
-    virtual void action() = 0;
-
-    /*Blocca l'esecuzione del task per @time millisecondi*/
-    void sleep(unsigned long time);
-
     /*Esegue il task. Questo metodo viene chiamato dallo Scheduler a ogni ciclo*/
-    void run();
+    virtual void run() = 0;
 };
 
 #endif
