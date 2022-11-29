926096ee2bccdc2ca4e3d5f432702866165d7ab1
Roboardo <mario@biavati.net>
2022-11-29 11:25:53 +0100

    yeee

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -8,10 +8,13 @@ void setup() {
   //init MSgService
   MsgService.init();
   //create basic components
-  Task* lct = new LedControlTask(new Led(2), new PirSensor(3), new LightSensor(A1,0.7));
-
+  Servo* servoMotor = new Servo();
+  servoMotor->attach(9);
+  Task* wlct = new WaterLevelControlTask(new Led(7), new Led(6), new Button(8), new Potentiometer(0), servoMotor, new Sonar(10, 11), new LiquidCrystal_I2C(0x27, 16, 2));
+  Task* lct = new LedControlTask(new Led(2), new PirSensor(3), new LightSensor(A1,0.7), (WaterLevelControlTask*)wlct);
   //create scheduler
   sch.addTask(lct);
+  sch.addTask(wlct);
 }
 
 void loop() {
